6e15dd5f52a61917d4535cc8cb104dfd9f0d6bbe
Fab M <lertiam@gmail.com>
2017-05-03 13:57:54 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
   # Get request to /users/:user_id/profile/new
   def new
   # Render blanck profile details form  
+  @profile = Profile.new
   end
 end
