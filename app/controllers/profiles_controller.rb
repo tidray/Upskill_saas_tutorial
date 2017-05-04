b80a8532acd585f7481419ae5b2fd36c4b46cf1d
Fab M <lertiam@gmail.com>
2017-05-04 09:51:56 +0000

    Added profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,33 +1,49 @@
 class ProfilesController < ApplicationController
-  # Get request to /users/:user_id/profile/new
-  def new
-  # Render blanck profile details form  
-  @profile = Profile.new
+    
+    # Get request to /users/:user_id/profile/new
+    def new
+    # Render blanck profile details form  
+    @profile = Profile.new
+    end
+    
+    # POST to /users/:user_id/profile
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
   end
   
-  # POST to /users/:user_id/profile
-def create
-  # Ensure that we have the user who is filling out form
-  @user = User.find( params[:user_id] )
-  # Create profile linked to this specific user
-  @profile = @user.build_profile( profile_params )
-  if @profile.save
-    flash[:success] = "Profile updated!"
-    redirect_to user_path( params[:user_id] )
-  else
-    render action: :new
+    # Get request to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
   end
-end
-
-  # Get request to /users/:user_id/profile/edit
-def edit
-  @user = User.find( params[:user_id] )
-  @profile = @user.profile
-end
-
-
-private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  
+    # PATCH to /users/:user_id/profile
+  def update
+    # Retrieve the user from the database
+    @user = User.find( params[:user_id] )
+    # Retrieve the user profile
+    @profile = @user.profile
+    # Mass assign edited profile attribute and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to  their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
   end
-end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+  end
